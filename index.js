5eb7e94150e2b61b2fc3dc893d0348e3b8791659
wangjiayususan1997 <wangjiayususan@gmail.com>
2023-12-09 10:17:02 -0800

    backend for login/logout complete

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(morgan("dev")); // log every request to the console
+app.use(express.json({ limit: "30mb", extended: true })); // used to parse incoming request bodies with JSON payloads. It parses the incoming JSON data and makes it available in the req.body object.
+app.use(express.urlencoded({ limit: "30mb", extended: true })); // used to parse incoming request bodies with URL-encoded payloads.
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:5000/users/signup
+
+const MONGODB_URL =
+  "mongodb+srv://wangjiayususan:AIjiayu0129@cluster0.onugccx.mongodb.net/meal?retryWrites=true&w=majority";
+
+const port = 5000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
