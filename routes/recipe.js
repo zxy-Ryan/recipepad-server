46925369192e616500159f2a86a2efb14c4d059f
tianyuanzoe <tian.yuan3@northeastern.edu>
2023-12-14 22:39:24 -0800

    add signout

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,11 +2,12 @@ import express from "express";
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
-import { createRecipe, getRecipes, getRecipe } from "../controllers/recipe.js";
+import { createRecipe, getRecipes, getRecipe,signout } from "../controllers/recipe.js";
 
 //methods diffrent, routes can be same
 router.get("/:id", getRecipe);
 router.get("/", getRecipes);
 router.post("/", auth, createRecipe);
+router.post("/signout", signout);
 
 export default router;
